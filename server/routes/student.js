18e201af358167fd05b625e729c90ae237068f96
hanhuikrkr <1084402065@qq.com>
2020-06-26 16:56:57 +0800

    完成虎鲸需求

--- server/routes/student.js
+++ server/routes/student.js
@@ -4,20 +4,54 @@ const express = require('express');
 const router = express.Router();
 const callProc = require('../util').callProc
 
-router.post('/getTopicList', async(req, res) => {
+router.post('/getTopicList', async (req, res) => {
     let sql = `CALL PROC_CAL_GET_STU_SELECT_TOPIC`;
-      let params = req.body;
-      callProc(sql, {}, res, (r) => {
-          res.status(200).json({code: 200, data: r, msg: '取课题列表'})
-      });
-  });
-  
-  router.post('/getStuInfo', async(req, res) => {
+    let params = req.body;
+    callProc(sql, {}, res, (r) => {
+        res.status(200).json({ code: 200, data: r, msg: '取课题列表' })
+    });
+});
+
+router.post('/getStuInfo', async (req, res) => {
     let sql = `CALL PROC_CAL_ONE_TOPIC(?)`;
-      let params = req.body;
-      console.log(params)
-      callProc(sql, params, res, (r) => {
-          res.status(200).json({code: 200, data: r, msg: '取学生课题'})
-      });
-  });
+    let params = req.body;
+    console.log(params)
+    callProc(sql, params, res, (r) => {
+        res.status(200).json({ code: 200, data: r, msg: '取学生课题' })
+    });
+});
+
+router.post('/addStuTopic', async (req, res) => {
+    let sql = `CALL PROC_ADD_ONE_STU_TOPIC(?)`;
+    let params = req.body;
+    console.log(params)
+    callProc(sql, params, res, (r) => {
+        res.status(200).json({ code: 200, data: r, msg: '增加学生课题' })
+    });
+});
+
+router.post('/delStuTopic', async (req, res) => {
+    let sql = `CALL PROC_DEL_ONE_STU_TOPIC(?)`;
+    let params = req.body;
+    console.log(params)
+    callProc(sql, params, res, (r) => {
+        res.status(200).json({ code: 200, data: r, msg: '删除学生课题' })
+    });
+});
+router.post('/getStuTopicStatus', async (req, res) => {
+    let sql = `CALL PROC_CAL_ONE_TOPIC_STATUS(?)`;
+    let params = req.body;
+    console.log(params)
+    callProc(sql, params, res, (r) => {
+        console.log(r)
+        if(r.length!=0)
+        {
+            res.status(200).json({ code: 200, data: r, msg: '取学生课题' })
+        }else{
+            res.status(200).json({ code: 200, data:"虎鲸┗|｀O′|┛ 嗷~~", msg: '取学生课题' })
+        }
+        
+    });
+});
+
 module.exports = router
