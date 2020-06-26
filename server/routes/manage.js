fe7e75443aa95bff1eb7b09c46f149e454b8cdbc
straightwood <2385471984@qq.com>
2020-06-26 14:22:55 +0800

    manage commit

--- server/routes/manage.js
+++ server/routes/manage.js
@@ -1,39 +1,37 @@
-
-
 const express = require('express');
 const router = express.Router();
-const callProc = require('../util').callProc
+const callProc = require('../util').callProc;
 
 router.post('/teacherList', async(req, res) => {
-  let sql = `CALL PROC_TEA_LIST_M`
-  let params = req.body
+  let sql = `CALL PROC_TEA_LIST_M`;
+  let params = req.body;
 	callProc(sql, {}, res, (r) => {
 		res.status(200).json({code: 200, data: r, msg: '取教师列表'})
-    })
-})
+    });
+});
 
 router.post('/topicList', async(req, res) => {
-  let sql = `CALL PROC_TOPIC_LIST_M`
-	let params = req.body
+  let sql = `CALL PROC_TOPIC_LIST_M`;
+	let params = req.body;
 	callProc(sql, {}, res, (r) => {
 		res.status(200).json({code: 200, data: r, msg: '取课题列表'})
-	})
-})
+	});
+});
 
 router.post('/checkAllocate', async(req, res) => {
-  let sql = `CALL PROC_CHECK_INSERT_M`
-	let params = req.body
+  let sql = `CALL PROC_CHECK_INSERT_M`;
+	let params = req.body;
 	callProc(sql, params, res, (r) => {
 		res.status(200).json({code: 200, data: r, msg: '插入课题审核id信息'})
-	})
-})
+	});
+});
 
 router.post('/checkList', async(req, res) => {
-  let sql = `CALL PROC_TOPIC_CHECK_LIST_M`
-	let params = req.body
+  let sql = `CALL PROC_TOPIC_CHECK_LIST_M`;
+	let params = req.body;
 	callProc(sql, {}, res, (r) => {
 		res.status(200).json({code: 200, data: r, msg: '取审核列表信息'})
-	})
-})
+	});
+});
 
 module.exports = router
