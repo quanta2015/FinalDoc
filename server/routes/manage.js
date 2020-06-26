6265013f099dc968c271ba75eb5ee524de0086fd
straightwood <2385471984@qq.com>
2020-06-26 14:18:30 +0800

    manage:审核列表

--- server/routes/manage.js
+++ server/routes/manage.js
@@ -10,7 +10,6 @@ router.post('/teacherList', async(req, res) => {
 	callProc(sql, {}, res, (r) => {
 		res.status(200).json({code: 200, data: r, msg: '取教师列表'})
     })
-    
 })
 
 router.post('/topicList', async(req, res) => {
@@ -21,5 +20,20 @@ router.post('/topicList', async(req, res) => {
 	})
 })
 
+router.post('/checkAllocate', async(req, res) => {
+  let sql = `CALL PROC_CHECK_INSERT_M`
+	let params = req.body
+	callProc(sql, params, res, (r) => {
+		res.status(200).json({code: 200, data: r, msg: '插入课题审核id信息'})
+	})
+})
+
+router.post('/checkList', async(req, res) => {
+  let sql = `CALL PROC_TOPIC_CHECK_LIST_M`
+	let params = req.body
+	callProc(sql, {}, res, (r) => {
+		res.status(200).json({code: 200, data: r, msg: '取审核列表信息'})
+	})
+})
 
 module.exports = router
