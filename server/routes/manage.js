84c4cf6e1cf6df84acb000504041e4c966852c2e
straightwood <2385471984@qq.com>
2020-06-26 23:21:11 +0800

    randAllocate接口

--- server/routes/manage.js
+++ server/routes/manage.js
@@ -19,13 +19,46 @@ router.post('/topicList', async(req, res) => {
 	});
 });
 
-router.post('/checkAllocate', async(req, res) => {
-  let sql = `CALL PROC_CHECK_INSERT_M(?)`;
-	let params = req.body;
-	callProc(sql, params, res, (r) => {
-		res.status(200).json({code: 200, data: r, msg: '插入课题审核id信息'})
+
+router.post('/randAllocate',async(req,res) => {
+  // let count = 5;
+  // let teacherId = "2020782";  //等待数据格式
+  let topicIdArr=[];
+  let result;
+  let sql = `CALL PROC_TOPIC_LIST_M`;
+
+  callProc(sql, {}, res, (r) => {
+		for(i of r){
+      topicIdArr.push(i);
+    }    
+    for(let i=0;i<count;i++){
+      let temp = topicIdArr[topicIdArr.length-1].tid;
+      if(temp==teacherId){
+        i--;
+      }
+      else{
+        let sql = `CALL PROC_CHECK_INSERT_M(?)`;
+        result={teacher_id:teacherId,topic_id:topicIdArr[topicIdArr.length-1].id};
+        topicIdArr.pop(); 
+        callProc(sql, result, res, (r) => {});
+      }
+    }
+    res.status(200).json({code: 200, msg: '自动课题审核分配信息'})
 	});
 });
+
+
+router.post('/checkAllocate', async(req, res) => {
+  for(let i of req.body){
+    for(let j of i.topic_id){
+      let sql = `CALL PROC_CHECK_INSERT_M(?)`;
+      let params = {teacher_id:i.teacher_id,topic_id:j};
+      callProc(sql, params, res, (r) => {});
+    }
+  }
+  res.status(200).json({code: 200,msg: '手动课题审核分配'})
+});
+
 
 router.post('/checkList', async(req, res) => {
   let sql = `CALL PROC_TOPIC_CHECK_LIST_M`;
@@ -35,5 +68,4 @@ router.post('/checkList', async(req, res) => {
 	});
 });
 
-// var json=JSON.stringify({teacher_id:"20021317",topic_id:""})
 module.exports = router
