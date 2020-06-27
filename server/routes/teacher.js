5fe6963f41732b03af1f6508c9c024abc0e73ed2
gitEast <linxiayoudongfeng@gmail.com>
2020-06-27 19:20:33 +0800

    +url传参使用json格式

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -1,6 +1,6 @@
 
 
-const url = require('url');
+// const url = require('url');
 const express = require('express');
 const router = express.Router();
 const callProc = require('../util').callProc
@@ -8,9 +8,7 @@ const callProc = require('../util').callProc
 // 由不完整学号得到学生列表（学号和姓名）：模糊查询
 router.post('/getStuInfoByLikeID', async(req, res) => {
     let sql = `CALL PROC_STUID_INFO_FUZZY(?)`;
-    var data = url.parse(req.url, true).query;
-    let params = {num: data.num};
-    // let params = req.body;
+    let params = req.body;
 	callProc(sql, params, res, (r) => {
         res.status(200).json({code: 200, data: r, msg: '取学生列表'});
 	})
@@ -19,8 +17,7 @@ router.post('/getStuInfoByLikeID', async(req, res) => {
   // 发布教师课题：向topic表中插入数据
   router.post('/postTopicInfo', async(req, res) => {
       let sql = `CALL PROC_TOPIC_INIT_INSERT(?)`;
-      var data = url.parse(req.url, true).query;
-      let params = {tea_id: data.tea_id, stuId: data.stuId, name: data.name, type: data.type, note: data.note};
+      let params = req.body;
       callProc(sql, params, res, (r) => {
         res.status(200).json({code: 200, data: r, msg: '插入数据成功'});
       })
@@ -29,8 +26,7 @@ router.post('/getStuInfoByLikeID', async(req, res) => {
   // 由教师id获取topic内容（课题id，课题状态）
   router.post('/getTidgetTopic', async(req, res) => {
     let sql = `CALL PROC_GET_TOPIC_STATUS(?)`;
-    var data = url.parse(req.url, true).query;
-    let params = {tea_id: data.tea_id};
+    let params = req.body;
     callProc(sql, params, res, (r) => {
         var status0 = 0;
         var status1 = 1;
@@ -56,8 +52,7 @@ router.post('/getStuInfoByLikeID', async(req, res) => {
   // 由课题pid获取课题内容（课题名称，课题类别，课题简介，学生id，学生名字）
   router.post('/getTopicFullInfo', async(req, res) => {
     let sql = `CALL PROC_PID_GET_TOPIC_INFO(?)`;
-    var data = url.parse(req.url, true).query;
-    let params = {pid: data.pid};
+    let params = req.body;
     callProc(sql, params, res, (r) => {
         res.status(200).json({code: 200, data: r, msg: '通过课题id课题查询成功，返回课题内容'});
     })
