f086844f2f8c270f0354d12413701131b6903498
hanhuikrkr <1084402065@qq.com>
2020-06-28 15:24:52 +0800

    新增删除

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -58,4 +58,13 @@ router.post('/getStuInfoByLikeID', async(req, res) => {
     })
   })
 
+  router.post('/getTopicFullInfo', async(req, res) => {
+    let sql = `CALL PROC_DEL_ONE_TOPIC(?)`;
+    let params = req.body;
+    callProc(sql, params, res, (r) => {
+        res.status(200).json({code: 200, data: r, msg: '通过课题id课题查询成功，返回课题内容'});
+    })
+  })
+
+
   module.exports = router;
