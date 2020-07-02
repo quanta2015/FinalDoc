23763733c408061d8aa6b25828f0dd287998856f
straightwood <2385471984@qq.com>
2020-07-02 16:57:29 +0800

    manage/handleGroup 开题答辩手动分配接口

--- server/routes/manage.js
+++ server/routes/manage.js
@@ -1,12 +1,25 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wyx
+ * @Date: 2020-06-27 21:54:36
+ * @LastEditors: wyx
+ * @LastEditTime: 2020-07-02 16:36:20
+ */ 
+
 const express = require('express');
 const router = express.Router();
 const db = require('../util/db');
 const callProc = require('../util').callProc;
 
-// const mysql = require('mysql');
-// const config = require("../util/conf.js")
-// const pool = mysql.createPool(config); 
 
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 获取全部教师列表
+ * @param {type} 
+ * @return: data
+ */
 router.post('/teacherList', async(req, res) => {
   let sql = `CALL PROC_TEA_LIST_M`;
   let params = req.body;
@@ -15,14 +28,28 @@ router.post('/teacherList', async(req, res) => {
     });
 });
 
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 获取未分配审核的课题列表
+ * @param {type} 
+ * @return: 
+ */
 router.post('/topicList', async(req, res) => {
   let sql = `CALL PROC_TOPIC_LIST_M`;
 	let params = req.body;
 	callProc(sql, {}, res, (r) => {
 		res.status(200).json({code: 200, data: r, msg: '取课题列表'})
-	});
+  });
 });
 
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 获取审核列表
+ * @param {type} 
+ * @return: 
+ */
 router.post('/checkList', async(req, res) => {
   let sql = `CALL PROC_TOPIC_CHECK_LIST_M`;
 	let params = req.body;
@@ -59,7 +86,13 @@ router.post('/checkList', async(req, res) => {
 //   return res.status(200).json({code: 200, msg: '自动课题审核分配'})
 // });
 
-
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 审核老师自动分配课题
+ * @param {type} 
+ * @return: 
+ */
 router.post('/randAllocate',async(req,res) => {
   let count = req.body[0];
   let message;
@@ -71,11 +104,16 @@ router.post('/randAllocate',async(req,res) => {
     let result = (message[0][0])
     resultArr.push(result);
   }
-  // console.log(resultArr);
   return res.status(200).json({code: 200, data: resultArr, msg: '自动课题审核分配'})
 });
 
-
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 给审核老师手动分配课题接口
+ * @param {type} 
+ * @return: 
+ */
 router.post('/checkAllocate', async(req, res) => {
   for(let i of req.body){
     for(let j of i.topic_id){
@@ -87,7 +125,13 @@ router.post('/checkAllocate', async(req, res) => {
   res.status(200).json({code: 200,msg: '手动课题审核分配'})
 });
 
-
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 审核相关数值
+ * @param {type} 
+ * @return: 未审核、未通过、已通过
+ */
 router.post('/auditCount', async(req, res) => {
   let sql = `CALL PROC_AUDIT_COUNT_M`;
 	let params = req.body;
@@ -96,13 +140,54 @@ router.post('/auditCount', async(req, res) => {
 	});
 });
 
-
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 取到领域表信息
+ * @param {type} 
+ * @return: 
+ */
 router.post('/areaList', async(req, res) => {
   let sql = `CALL PROC_GET_AREA_LIST_M`;
 	let params = req.body;
 	callProc(sql, {}, res, (r) => {
 		res.status(200).json({code: 200, data: r, msg: '取领域列表'})
 	});
+});
+
+/**
+ * @name: 
+ * @test: test font
+ * @msg: 开题答辩 手动分组
+ * @param {type} 
+ * @return: 
+ */
+router.post('/handleGroup', async(req, res) => {
+  let teacher_char = "";
+  let teacher_len = 0;
+  let topic_char = "";
+  let topic_len = 0;
+
+  for(let i of req.body.teacher_id){
+    teacher_char += (i+",");
+    teacher_len++;
+  }
+  teacher_char = teacher_char.substring(0, teacher_char.length - 1);
+  for(let i of req.body.topic_id){
+    topic_char += (i+",");
+    topic_len++;
+  }
+  topic_char = topic_char.substring(0, topic_char.length - 1);
+  
+  let sql = `CALL PROC_HANDLE_GROUP_M(?)`;
+    let params = {leader_id:req.body.leader_id,
+                  teacher_id:teacher_char,
+                  teacher_len:teacher_len,
+                  topic_id:topic_char,
+                  topic_len:topic_len};
+    callProc(sql, params, res, (r) => {
+      res.status(200).json({code: 200,msg: '开题答辩手动分组成功'})
+    });
 });
 
 
