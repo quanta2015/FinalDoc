e237267a10110ca6bfc2ddae76cf692fbf509cad
straightwood <2385471984@qq.com>
2020-07-07 13:42:25 +0800

    系主任端获取文件相关接口

--- server/routes/auditMg.js
+++ server/routes/auditMg.js
@@ -4,7 +4,7 @@
  * @Author: wyx
  * @Date: 2020-07-06 11:00:47
  * @LastEditors: wyx
- * @LastEditTime: 2020-07-06 20:25:54
+ * @LastEditTime: 2020-07-07 13:33:40
  */ 
 
 const express = require('express');
@@ -15,7 +15,7 @@ const callProc = require('../util').callProc;
 /**
  * @name: 
  * @test: test font
- * @msg: 查看课题进度
+ * @msg: 查看课题进度 --本系
  * @param {ide:String} 
  * @return: 
  */
@@ -23,8 +23,40 @@ router.post('/viewProgress', async(req, res) => {
     let sql = `CALL MG_A_PROC_VIEW_PROGRESS(?)`;
       let params = req.body;
       callProc(sql, params, res, (r) => {
-          res.status(200).json({code: 200, data: r, msg: '发布课题判断'})
+          res.status(200).json({code: 200, data: r, msg: '查看本系课题进度'})
       });
   });
 
+  
+/**
+  * @name: 
+  * @test: test font
+  * @msg: 课题文件相关字段接口
+  * @param {topic_id:int} 
+  * @return: 
+  */
+ router.post('/viewFiles', async(req, res) => {
+     let sql = `CALL MG_A_PROC_VIEW_FILES(?)`;
+       let params = req.body;
+       callProc(sql, params, res, (r) => {
+           res.status(200).json({code: 200, data: r, msg: '查看该课题文件'})
+       });
+   });
+
+   /**
+  * @name: 
+  * @test: test font
+  * @msg: 课题任务书列表 --本系
+  * @param {ide:String} 
+  * @return: 
+  */
+ router.post('/taskList', async(req, res) => {
+    let sql = `CALL MG_A_PROC_REVIEW_TASK(?)`;
+      let params = req.body;
+      callProc(sql, params, res, (r) => {
+          res.status(200).json({code: 200, data: r, msg: '查看本系任务书列表'})
+      });
+  });
+
+   
   module.exports = router
