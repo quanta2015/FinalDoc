743cfe1e4237b37b5c1ede14b541133eb7d51b1a
gitEast <1722447547@qq.com>
2020-07-07 17:31:07 +0800

    +修改可用

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -1,12 +1,7 @@
-
-
-// const url = require('url');
+const url = require('url');
 const express = require('express');
 const router = express.Router();
 const callProc = require('../util').callProc;
-const formidable = require('formidable');
-let path = require("path");
-const fs = require('fs');
 
 // 由不完整学号得到学生列表（学号和姓名）：模糊查询
 router.post('/getStuInfoByLikeID', async(req, res) => {
@@ -137,6 +132,16 @@ router.post('/getTeacherAreas', async(req, res) => {
     let sql = `CALL PROC_GET_TEACHER_AREAS(?)`;
     let params = req.body;
     callProc(sql, params, res, (r) => {
+        var areas = r[0].area_list.split(',');
+        r = [];
+        for (let i = 0; i < areas.length; i++) {
+            const element = areas[i].split('|');
+            var item = {}; 
+            item.id = parseInt(element[0]);
+            item.area = element[1];
+            item.color = element[2];
+            r.push(item);
+        }
         res.status(200).json({code: 200, data: r, msg: '教师研究方向数组已返回'});
     })
 })
@@ -187,56 +192,9 @@ router.post('/getStudentUntied', async(req, res) => {
     })
 })
 
-// 上传签名文件
-// router.post('/upload', async function (req, res) {
-//     const form = new formidable.IncomingForm()
-//     form.uploadDir = "./upload/";
-//     let get_data, newpath;
-//     form.parse(req, (err, fields, files) => {
-//         if (err || !files.file) {
-//             res.status(500);
-//         }
-//         get_data = fields;
-//         let ext = files.file.name.split('.').slice(-1)
-//         let ttt = `${get_data.tid}_签名_${moment(new Date()).format('YYYYMMDDhhmmss')}.${ext}`;
-//         let oldpath = files.file.path
-//         console.log("===============旧地址====================", oldpath)
-//         newpath = "./upload/" + ttt;
-//         console.log("===============新地址====================", newpath)
-//         // console.log(oldpath, newpath)
-//         fs.rename(oldpath, newpath, function (err) {
-//             if (err) {
-//                 console.log(err);
-//                 throw Error("改名失败");
-//             }
-//         });
-//         let sql = `CALL PROC_UPDATE_TOPIC_DATA(?)`;
-//         get_data.filePath = newpath;
-//         callProc(sql, get_data, res, (r) => {
-//             res.status(200).json({
-//                 code: 200,
-//                 msg: `上传签名成功`,
-//                 data: newpath,
-//                 mysqldata: r
-//             })
-//         });
-//     });
-// })
-
-// // 返回签名文件位置
-// router.post('/getTeacherSignPath', async(req, res) => {
-//     let sql = `CALL PROC_GET_TOPICID_SIGN_PATH(?)`;
-//     let params = req.body;
-//     callProc(sql, params, res, (r) => {
-//         res.status(200).json({code: 200, data: r, msg: '签名文件位置已返回'});
-//     })
-// })
-
-// // 下载签名文件
-// router.post('/download', function (req, res, next) {
-//     let filename = req.body.file
-//     console.log(res);
-//     res.download('./' + filename)
+// 上传教师签名文件
+// router.post('/uploadSign', async(req, res) => {
+//     let sql = `CALL PROC_UPDATE_USER_SIGN`;
 // })
 
 module.exports = router;
