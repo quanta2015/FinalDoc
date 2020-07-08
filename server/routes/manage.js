dfb6497240f9b34273e17cb2aa2a2429414e8f17
straightwood <2385471984@qq.com>
2020-07-08 20:55:44 +0800

    系主任接口更改

--- server/routes/manage.js
+++ server/routes/manage.js
@@ -4,7 +4,7 @@
  * @Author: wyx
  * @Date: 2020-06-27 21:54:36
  * @LastEditors: wyx
- * @LastEditTime: 2020-07-08 20:28:44
+ * @LastEditTime: 2020-07-08 20:55:25
  */ 
 
 const express = require('express');
@@ -55,12 +55,12 @@ router.post('/checkList', async(req, res) => {
 	let params = req.body;
 	callProc(sql, params, res, (r) => {
     r.forEach(element => {
-      if (!element.result && element.sugg===null){
+      if (element.sel!=-1 && !element.result && element.sugg===null){
         element.result=2; //待审核
       }else{
-        if(element.stuId===null){
+        if(element.sel===-1 && element.stuId===null){
           element.result=3; //没有选定学生时--待选
-        }else{
+        }if(element.sel===-1 && element.stuId!=null){
           element.result=4; //学生选定成功--已选
         }
       }
