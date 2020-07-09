6504cd67e554142a6a141e774c24d4889ea2ad53
gitEast <1722447547@qq.com>
2020-07-09 10:37:38 +0800

    +注释

---
+++ server/routes/admin.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 第二阶段 管理员端接口
+ * @version: 1.0
+ * @Author: East Wind
+ * @Date: 2020-07-09 10:14:36
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2020-07-09 10:37:18
+ */ 
+
+const express = require('express');
+const router = express.Router();
+const callProc = require('../util').callProc;
