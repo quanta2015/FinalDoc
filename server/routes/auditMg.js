2ac5acd423efe9b22e43003dea51062ddfa902ac
straightwood <2385471984@qq.com>
2020-07-18 08:52:18 +0800

    系主任审核任务书

--- server/routes/auditMg.js
+++ server/routes/auditMg.js
@@ -4,7 +4,7 @@
  * @Author: wyx
  * @Date: 2020-07-06 11:00:47
  * @LastEditors: wyx
- * @LastEditTime: 2020-07-07 13:33:40
+ * @LastEditTime: 2020-07-18 08:51:07
  */ 
 
 const express = require('express');
@@ -43,7 +43,7 @@ router.post('/viewProgress', async(req, res) => {
        });
    });
 
-   /**
+/**
   * @name: 
   * @test: test font
   * @msg: 课题任务书列表 --本系
@@ -51,12 +51,35 @@ router.post('/viewProgress', async(req, res) => {
   * @return: 
   */
  router.post('/taskList', async(req, res) => {
-    let sql = `CALL MG_A_PROC_REVIEW_TASK(?)`;
+    let sql = `CALL MG_A_PROC_TASK_LIST(?)`;
       let params = req.body;
       callProc(sql, params, res, (r) => {
           res.status(200).json({code: 200, data: r, msg: '查看本系任务书列表'})
       });
   });
 
+  /**
+  * @name: 
+  * @test: test font
+  * @msg: 审核任务书
+  * @param {topic_id:[int]} 
+  * @return: 
+  */
+ router.post('/reviewTask', async(req, res) => {
+    let topic_char = "";
+    let topic_len = 0;
+    for(let i of req.body.topic_id){      //数据格式处理
+      topic_char += (i+",");
+      topic_len++;
+    }
+    topic_char = topic_char.substring(0, topic_char.length - 1);
+
+    let sql = `CALL MG_A_PROC_REVIEW_TASK(?)`;
+    let params = {topic_id:topic_char,
+                topic_len:topic_len};
+    callProc(sql, params, res, (r) => {
+        res.status(200).json({code: 200, data: r, msg: '审核任务书'})
+    });
+  });
    
   module.exports = router
