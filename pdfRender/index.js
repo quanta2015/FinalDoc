9ab201d824a8f1940a9df7731593004cac93d30f
hanhuikrkr <1084402065@qq.com>
2020-07-26 01:33:37 +0800

    merge linux Compatible

--- pdfRender/index.js
+++ pdfRender/index.js
@@ -5,6 +5,23 @@ const puppeteer = require('puppeteer');
 const callProc = utils.callProc;
 const sleep = utils.sleep;
 const app = express();
+function getCurrentDate() {
+  var date = new Date();
+  var seperator1 = "-";
+  var seperator2 = ":";
+  var month = date.getMonth() + 1;
+  var strDate = date.getDate();
+  if (month >= 1 && month <= 9) {
+    month = "0" + month;
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = "0" + strDate;
+  }
+  var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+    + " " + date.getHours() + seperator2 + date.getMinutes()
+    + seperator2 + date.getSeconds();
+  return currentdate;
+}
 //设置静态文件
 app.use(express.static('public'))
 //用作浏览器访问页面
@@ -16,14 +33,17 @@ app.get('/', (req, res) => {
 let data;
 let i = 0;
 app.get('/getStat', (req, res) => {
-  let task = fs.readFileSync(data[i].file, "utf-8");
+  let task = fs.readFileSync(data[i].file.replace("./upload","/root/upload"), "utf-8");
+  console.log("获取的数据为：",data)
   data[i].task = task
   res.status(200).json({ code: 200, data: data[i++] })
 })
 
 //开启服务
 let server = app.listen(9999, () => {
+  console.log("我起床了")
   console.log("listening。。");
+  console.log(getCurrentDate());
   let sql = "call PROC_RENDER_PDF()";
   callProc(sql, null, null, async r => {
     //本地测试使用
@@ -32,7 +52,8 @@ let server = app.listen(9999, () => {
     // }
     data = r;
     await rendPDF();
-    process.exit();
+    await sleep(7600000)
+    
   })
 })
 //保存task
@@ -70,9 +91,10 @@ rendPDF = async () => {
     let pid = data[j].pid;
     await page.goto('http://localhost:9999');
     await sleep(2000);
+    console.log(path)
     await page.pdf(
       {
-        path: path,
+        path: path.replace("./upload","/root/upload"),
       });
     console.log(`printed ${j + 1} pages SUCCESS!!, now saving to DB`);
     await saveToDB({ pid: pid, filePath: path });
