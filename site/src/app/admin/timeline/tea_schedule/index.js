db4da07e133658b13bc3bfddeb171d032fa86af0
hanhuikrkr <1084402065@qq.com>
2020-08-07 05:00:58 +0800

    管理端：时间进度->暂存

--- site/src/app/admin/timeline/tea_schedule/index.js
+++ site/src/app/admin/timeline/tea_schedule/index.js
@@ -36,6 +36,12 @@ const { RangePicker } = DatePicker;
 @inject("userStore")
 @observer
 export default class scheduleSet extends Component {
+  state = {
+    majorSelected: false,
+    major: "",
+    timelineData: null,
+    timeOption: [],
+  };
   @computed
   get usr() {
     return toJS(this.props.userStore.usr);
@@ -45,19 +51,49 @@ export default class scheduleSet extends Component {
     console.log("this.usr.uid");
     if (!this.usr.uid) route("/");
   }
-  onRangeChange=(date, dateString)=> {
+  onRangeChange = (date, dateString) => {
     console.log(dateString);
-  }
+  };
+  onSelectChange = (value) => {
+    this.setState({ majorSelected: true });
+    let param = {
+      major: value,
+      role: 1,
+    };
+
+    console.log(`selected ${param}`);
+  };
 
   render() {
     console.log("========管理端系统设置->时间进度设定 界面页面===========");
+    const { majorSelected, major, timeOption, timelineData } = this.state;
     return (
       <div>
         <Row gutter={[0, 0]}>
           <Col span={8}>
-            <div className="admin-content">
+            <div>
               <div>管理端系统设置->时间进度设定 界面页面</div>
-              <RangePicker onChange={this.onRangeChange} renderExtraFooter={() => "extra footer"} />
+              <Select
+                placeholder="请选择专业系"
+                style={{ width: 278 }}
+                onChange={this.onSelectChange}
+                size="middle"
+              >
+                <Option value="计算机系">计算机系</Option>
+                <Option value="金融系">金融系</Option>
+                <Option value="物联网软工系">物联网软工系</Option>
+              </Select>
+              {majorSelected && (
+                <div>
+                  <div>开题报告：</div>
+                  <div>
+                    <RangePicker
+                      onChange={this.onRangeChange}
+                      renderExtraFooter={() => "extra footer"}
+                    />
+                  </div>
+                </div>
+              )}
             </div>
           </Col>
           <Col span={1}></Col>
