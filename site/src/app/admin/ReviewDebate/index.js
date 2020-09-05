1d7b30e79718d9131f6a94f78a7b40a7640e5e7d
hanhuikrkr <1084402065@qq.com>
2020-09-05 21:31:54 +0800

    merge管理端冲突项目

--- site/src/app/admin/ReviewDebate/index.js
+++ site/src/app/admin/ReviewDebate/index.js
@@ -1,20 +1,17 @@
+import { Component } from "preact";
+import { inject, observer } from "mobx-react";
+import { computed, toJS } from "mobx";
+import { route } from "preact-router";
 
-import { Component } from 'preact';
-import { inject, observer } from 'mobx-react';
-import { computed, toJS } from 'mobx';
-import { route } from 'preact-router';
-
-import { Tabs, Radio, Button } from 'antd';
-
+import { Tabs, Radio, Button } from "antd";
 const { TabPane } = Tabs;
 
-@inject('userStore')
+@inject("userStore")
 @observer
 export default class reviewdebate extends Component {
-
   state = {
-    value: 1,
-  }
+    normal: true,
+  };
 
   @computed
   get usr() {
@@ -23,20 +20,77 @@ export default class reviewdebate extends Component {
 
   componentDidMount() {
     if (!this.usr.uid) {
-      route('/')
+      route("/");
     }
   }
 
   // 切换自动手动单选框
 
-
+  onChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      normal: e.target.value,
+    });
+  };
   render() {
+    const { normal } = this.state;
     return (
       <>
-        <div>
-            <spam>
-                评阅答辩界面
-            </spam>
+        <div className="admin-content">
+          <div className="g-ann">
+            <Tabs defaultActiveKey="1">
+              <TabPane tab="开题答辩" key="open">
+                <div className="m-choose">
+                  <Radio.Group
+                    onChange={this.onChange}
+                    value={normal}
+                    style={{ marginBottom: 20 }}
+                  >
+                    <Radio.Button value={true}>正常答辩</Radio.Button>
+                    <Radio.Button value={false}>延期答辩</Radio.Button>
+                  </Radio.Group>
+                  <Button
+                    className="m-release-btn"
+                    onClick={this.handleAdd}
+                    type="primary"
+                    style={{
+                      marginBottom: 16,
+                    }}
+                  >
+                    添加信息
+                  </Button>
+                </div>
+                {normal === true && <div>开题答辩->正常答辩</div>}
+                {normal === false && <div>开题答辩->延期答辩</div>}
+              </TabPane>
+
+              <TabPane tab="毕设答辩" key="final">
+                <div className="m-choose">
+                  <Radio.Group
+                    onChange={this.onChange}
+                    value={this.state.value}
+                    style={{ marginBottom: 20 }}
+                  >
+                    <Radio.Button value={true}>正常答辩</Radio.Button>
+                    <Radio.Button value={false}>延期答辩</Radio.Button>
+                  </Radio.Group>
+                  <Button
+                    className="m-release-btn"
+                    onClick={this.handleAdd}
+                    type="primary"
+                    style={{
+                      marginBottom: 16,
+                    }}
+                  >
+                    添加信息
+                  </Button>
+                </div>
+                {normal === true && <div>开题答辩->正常答辩</div>}
+                {normal === false && <div>开题答辩->延期答辩</div>}
+              </TabPane>
+            </Tabs>
+            {/* <div className="m-m">公告模块</div> */}
+          </div>
         </div>
       </>
     );
